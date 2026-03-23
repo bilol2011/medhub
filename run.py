4c6c053463713aff7ac1c8599d8cafb1a202af27
bilol2011 <bilolmahamadjonov55@gmail.com>
2026-03-23 21:33:29 +0500

    https://github.com/bilol2011/medhub.git

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ app = create_app()
 
 if __name__ == '__main__':
     if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
-        webbrowser.open('http://127.0.0.1:5000')
-    socketio.run(app, debug=True, host='0.0.0.0')
+        webbrowser.open('http://192.168.89.229:5050')
+    socketio.run(app, debug=True, host='192.168.89.229', port=5050)
